cc4b7166358270d4d02c81e9a7c1ead6e895063b
jdl <joedlyman@gmail.com>
2022-03-02 21:44:16 +0000

    Consolidate user of version string and apply Black formatting

--- setup.py
+++ setup.py
@@ -1,27 +1,22 @@
 import setuptools
 
+from pympc import __version__
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pympc',
-    version='0.6.1',
-    author='Joe Lyman',
-    description='minor planet checking',
+    name="pympc",
+    version=__version__,
+    author="Joe Lyman",
+    description="minor planet checking",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "ephem",
-        "astropy>=3.2",
-        "pandas>=1.0",
-        "requests>=2.0",
-    ],
-    license='GNU General Public License v3 (GPLv3)',
+    install_requires=["ephem", "astropy>=3.2", "pandas>=1.0", "requests>=2.0"],
+    license="GNU General Public License v3 (GPLv3)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lyalpha/pympc",
     download_url="https://github.com/Lyalpha/pympc/archive/v0.6.1.tar.gz",
-    classifiers=[
-         "Programming Language :: Python :: 3",
-    ],
-    python_requires='>=3.6',
+    classifiers=["Programming Language :: Python :: 3"],
+    python_requires=">=3.6",
 )
