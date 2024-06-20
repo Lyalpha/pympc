01621bf3fa972cb7c7b93f4dc6ebd4cdfea04761
jdl <joedlyman@gmail.com>
2024-06-20 17:02:08 +0100

    Upgrade ephem minimum version requirement

--- setup.py
+++ setup.py
@@ -8,12 +8,14 @@ setuptools.setup(
     author="Joe Lyman",
     description="minor planet checking",
     packages=setuptools.find_packages(),
-    install_requires=["ephem>=3.7.7.1", "astropy>=4.2", "pandas>=1.0", "pyerfa>=2.0.0.0"],
+    install_requires=["ephem>=4.1", "astropy>=4.2", "pandas>=1.0", "pyerfa>=2.0.0.0"],
     license="GNU General Public License v3 (GPLv3)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lyalpha/pympc",
-    entry_points={"console_scripts": ["minor_planet_check=pympc.pympc:_console_script"]},
+    entry_points={
+        "console_scripts": ["minor_planet_check=pympc.pympc:_console_script"]
+    },
     classifiers=["Programming Language :: Python :: 3"],
     python_requires=">=3.6",
     include_package_data=True,
